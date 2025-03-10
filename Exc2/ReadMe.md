e67af7f7ad0ddb077eed76cf0ef0c897f53d9bb7
a.bardanov <a.bardanov@yablochkovtech.com>
2025-03-10 20:05:57 +0300

    sprint6: Добавил скриншоты масштабирования и логи

---
+++ Exc2/ReadMe.md
@@ -0,0 +1,7 @@
+# Результаты выполнения второго задания
+
+- Скриншоты панелей в директории `./screenshots` (получилось отмасштабироваться автоматчески до 3-х подов)
+- [Лог Kubernetes](./events.log)
+- [Лог Locust](./locust.log)
+- Манифесты Kubernetes в файлах `deployment.yaml`, `service.yaml`, `hpa.yaml`
+- [Баш скрипт](./run-scale-test-app.sh) для запуска кластера Kubernetes 
--- Exc2/run-scale-test-app.sh
+++ Exc2/run-scale-test-app.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-minikube start --addons=metrics-server --vm-driver=<Назавние драйвера> # у меня драйвер kvm2
-kubectl apply -f ./Exc2/deployment.yaml 
-kubectl apply -f ./Exc2/service.yaml 
-kubectl apply -f ./Exc2/hpa.yaml 
+minikube start --addons=metrics-server --vm-driver=kvm2 # тут может быть другой драйвер
+kubectl apply -f ./deployment.yaml 
+kubectl apply -f ./service.yaml 
+kubectl apply -f ./hpa.yaml 
 
 minikube service scale-test-app-service --url
